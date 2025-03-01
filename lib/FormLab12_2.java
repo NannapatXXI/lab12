0d37f65eded2e26a2e1c15a52c047a32476b3e1a
XXI <152409931+NannapatXXI@users.noreply.github.com>
2025-03-01 19:02:28 +0700

    Update FormLab12_2.java

--- lib/FormLab12_2.java
+++ lib/FormLab12_2.java
@@ -1,17 +1,13 @@
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 import javax.swing.*;
 
 public class FormLab12_2 extends JFrame implements MouseListener{
-
     Container cp ;
     JButton allBT[] ;
     JLabel L ;
     int count = 0 ;
-    
-   
     public FormLab12_2(){
         Initial();
         setComponent();
@@ -23,15 +19,13 @@ public class FormLab12_2 extends JFrame implements MouseListener{
         allBT = new JButton[400];
     }
     public void setComponent(){
-
         JPanel p = new JPanel();
         p.setLayout(new GridLayout(20,20));
-
         for(int i=0 ; i<400 ; i++){
             allBT[i] = new JButton();
             allBT[i].setBackground(Color.white);
-            allBT[i].setOpaque(true);
-            allBT[i].setBorderPainted(false);
+         //   allBT[i].setOpaque(true); for mac
+           //  allBT[i].setBorderPainted(false);  for mac
             allBT[i].setPreferredSize(new Dimension(20, 20));
             allBT[i].addMouseListener(this);
             p.add(allBT[i]);
@@ -44,23 +38,16 @@ public class FormLab12_2 extends JFrame implements MouseListener{
         cp.add(L,BorderLayout.NORTH);
         cp.add(p,BorderLayout.CENTER);
     }
-    
-   
-      
     public void Finally(){
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
     Color tmp;
-
     public void mouseClicked(MouseEvent e) {}
     public void mousePressed(MouseEvent e) {}
-   
     public void mouseReleased(MouseEvent e) {
-       
                 if (tmp == Color.red) {
                     e.getComponent().setBackground(Color.white);
                     tmp = Color.white;
@@ -73,10 +60,8 @@ public class FormLab12_2 extends JFrame implements MouseListener{
     }
     public void mouseEntered(MouseEvent e) {
         tmp = e.getComponent().getBackground();
-       
         e.getComponent().setBackground(Color.black);
     }
-   
     public void mouseExited(MouseEvent e) {
         e.getComponent().setBackground(tmp);
     }
